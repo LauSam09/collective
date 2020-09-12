98dc0d66e14e5fa946473ffadb7cd13ddd6abe64
Laurence Ede <laurence.ede@gmail.com>
2020-09-12 20:47:35 +0100

    Add lint and fix lint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
         <p>Collective</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
