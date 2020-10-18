fce8a312a5bdebde97d8c49bbc9a243a4c330686
Laurence Ede <laurence.ede@gmail.com>
2020-10-18 19:28:42 +0100

    Add close button

--- src/components/Lists/EditModal.tsx
+++ src/components/Lists/EditModal.tsx
@@ -1,3 +1,5 @@
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Item } from "models"
 import React from "react"
 import Modal from "react-modal"
@@ -13,9 +15,14 @@ export default function EditModal(props: Props) {
   const { item, close } = props
   return (
     <Modal isOpen={true} onRequestClose={close} className={classes.modal}>
-      <div>
-        <h3>{item.name}</h3>
-      </div>
+      <section className={classes.modalBody}>
+        <div className={classes.header}>
+          <h3>{item.name}</h3>
+          <button onClick={close}>
+            <FontAwesomeIcon icon={faWindowClose} />
+          </button>
+        </div>
+      </section>
     </Modal>
   )
 }
