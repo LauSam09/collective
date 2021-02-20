d9d95ea1424d267eb7bc616b684d28f45a5b7543
Laurence Ede <laurence.ede@gmail.com>
2021-02-20 14:59:23 +0000

    Add poc theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import logo from "./logo.svg"
-import "./App.css"
+import { useEffect, useState } from "react"
+import classes from "./App.module.css"
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light")
+  const nextTheme = theme === "light" ? "dark" : "light"
+
+  useEffect(() => {
+    document.body.dataset.theme = theme
+  }, [theme])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={classes.app}>Collective</div>
+      <button onClick={() => setTheme(nextTheme)}>
+        Change to {nextTheme} mode
+      </button>
+    </>
   )
 }
 
