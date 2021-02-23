5af51dd26c0eceb2773b8dc892ee55f6259064df
Laurence Ede <laurence.ede@gmail.com>
2021-02-23 21:13:50 +0000

    Reenable persistence conditionally to fix tests

--- src/Config/database.ts
+++ src/Config/database.ts
@@ -1,6 +1,10 @@
+import { env } from "process"
 import firebase from "firebase/app"
 
 import fbConfig from "Config/firebase.json"
 
 firebase.initializeApp(fbConfig)
-// firebase.firestore().enablePersistence({ synchronizeTabs: true })
+
+if (env.NODE_ENV !== "test") {
+  firebase.firestore().enablePersistence({ synchronizeTabs: true })
+}
