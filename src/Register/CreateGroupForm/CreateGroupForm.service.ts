975046943a09c7c4a6f5fad3a476883da5214343
Laurence Ede <laurence.ede@gmail.com>
2021-02-24 21:14:09 +0000

    Add list & category generation to group creation

--- src/Register/CreateGroupForm/CreateGroupForm.service.ts
+++ src/Register/CreateGroupForm/CreateGroupForm.service.ts
@@ -2,19 +2,72 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 import { Group } from "Authentication"
+import { Category } from "Lists/models"
 
 const db = firebase.firestore()
 
+// For now hard-code the initial categories. These could be customisable in future.
+const defaultCategories: Category[] = [
+  {
+    name: "Fruit & Vegetables",
+    colour: "#74ac72",
+    order: 0,
+  },
+  {
+    name: "Meat",
+    colour: "#ffb0b0",
+    order: 1,
+  },
+  {
+    name: "Fresh",
+    colour: "#95d5b7",
+    order: 2,
+  },
+  {
+    name: "Pantry",
+    colour: "#969B96",
+    order: 3,
+  },
+  {
+    name: "Drinks",
+    colour: "#a4c7db",
+    order: 4,
+  },
+  { name: "Eggs & Dairy", colour: "#fcf403", order: 5 },
+  {
+    name: "Health & Beauty",
+    colour: "#ff99ff",
+    order: 6,
+  },
+  {
+    name: "Home",
+    colour: "#487586",
+    order: 7,
+  },
+  { name: "Frozen", colour: "#0384fc", order: 8 },
+]
+
 export async function createGroup(group: Group) {
-  const doc = await db.collection("groups").add(group)
-  // TODO add list collection so we can add default list id below
+  const groupDoc = await db.collection("groups").add(group)
+  const listDoc = await groupDoc.collection("lists").add({ name: "shopping" })
+
+  const batch = db.batch()
+
+  defaultCategories.forEach((category) => {
+    const categoryRef = listDoc.collection("categories").doc()
+    batch.set(categoryRef, category)
+  })
+
+  await batch.commit()
+
   await db
     .collection("users")
     .doc(group.users[0])
     .update({
       group: {
-        id: doc.id,
+        id: groupDoc.id,
         name: group.name,
+        defaultList: listDoc.id,
       },
     })
 }
