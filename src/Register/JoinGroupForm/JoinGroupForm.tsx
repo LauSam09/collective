373fe7f208458008d786a98137bcaf1de64671c9
Laurence Ede <laurence.ede@gmail.com>
2021-02-28 17:33:48 +0000

    Replace join form with displayed id

--- src/Register/JoinGroupForm/JoinGroupForm.tsx
+++ src/Register/JoinGroupForm/JoinGroupForm.tsx
@@ -1,41 +1,12 @@
-import { useForm } from "react-hook-form"
-
-import classes from "./JoinGroupForm.module.css"
-
-type Form = {
-  joinKey: string
-}
+import { useUser } from "Authentication"
 
 export function JoinGroupForm() {
-  const { formState, register } = useForm<Form>({
-    defaultValues: {
-      joinKey: "",
-    },
-    mode: "onChange",
-  })
-
-  const { errors, isValid } = formState
-
   return (
     <div>
       <p>
         Alternatively, if someone else in your household has already signed up,
-        then enter the key they've provided here.
+        then ask them to invite you with this key: <span>{useUser()?.id}</span>
       </p>
-      <form>
-        <div className={classes.formGroup}>
-          <input
-            name="joinKey"
-            ref={register({ required: "You must provide a value" })}
-          />
-          <span className={classes.error}>
-            {errors?.joinKey ? errors.joinKey.message : null}
-          </span>
-          <button className={classes.submit} disabled={!isValid} type="submit">
-            Join
-          </button>
-        </div>
-      </form>
     </div>
   )
 }
