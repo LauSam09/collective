524719d19f3344f843f5314cfd4830081ab57bc6
Laurence Ede <laurence.ede@gmail.com>
2021-03-01 18:36:41 +0000

    Remove unused vars

--- src/Authentication/UserContext.test.tsx
+++ src/Authentication/UserContext.test.tsx
@@ -1,6 +1,5 @@
-import { screen, render } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import renderer from "react-test-renderer"
-import { AuthContext } from "./AuthContext"
 
 import { UserProvider } from "./UserContext"
 
