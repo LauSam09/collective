986d356daea5ad59d0faa6c7b5fa2fd2c347fe6f
Laurence Ede <laurence.ede@gmail.com>
2021-03-01 20:04:16 +0000

    Add react-router-dom package

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -1,4 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"
+
 import { useAuth, useIsAuthenticated } from "Authentication"
+
 import classes from "./Header.module.css"
 
 export default function Header() {
@@ -8,7 +12,11 @@ export default function Header() {
     <header className={classes.header}>
       <nav>
         <a href="/">Collective</a>
-        {useIsAuthenticated() ? <button onClick={logout}>Logout</button> : null}
+        {useIsAuthenticated() ? (
+          <button onClick={logout} title="Log out" className={classes.logout}>
+            <FontAwesomeIcon icon={faSignOutAlt} size="lg" />
+          </button>
+        ) : null}
       </nav>
     </header>
   )
