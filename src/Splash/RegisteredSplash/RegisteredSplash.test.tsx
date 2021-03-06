a43bd51e4e72b25209e4b87e4821c2f88412c80f
Laurence Ede <laurence.ede@gmail.com>
2021-03-06 19:02:20 +0000

    Add placeholder values for registered splash

--- src/Splash/RegisteredSplash/RegisteredSplash.test.tsx
+++ src/Splash/RegisteredSplash/RegisteredSplash.test.tsx
@@ -1,3 +1,13 @@
+import { render } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import { RegisteredSplash } from "./RegisteredSplash"
+
 describe("<RegisteredSplash />", () => {
-  test.todo("renders")
+  test("renders", () => {
+    render(
+      <BrowserRouter>
+        <RegisteredSplash />
+      </BrowserRouter>
+    )
+  })
 })
--- src/Splash/RegisteredSplash/RegisteredSplash.tsx
+++ src/Splash/RegisteredSplash/RegisteredSplash.tsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom"
+
+import classes from "./RegisteredSplash.module.css"
+
 export function RegisteredSplash() {
-  return <>Hello</>
+  return (
+    <article>
+      <ListSummary />
+      <MealPlanningSummary />
+    </article>
+  )
+}
+
+function ListSummary() {
+  return (
+    <section className={classes.section}>
+      <div>You have 15 items in Shopping</div>
+      <Link to="/lists">Go to list</Link>
+    </section>
+  )
+}
+
+function MealPlanningSummary() {
+  return (
+    <section className={classes.section}>
+      <div>You've planned meals for 3 days this week</div>
+      <Link to="/planning">Go to meal planning</Link>
+    </section>
+  )
 }
