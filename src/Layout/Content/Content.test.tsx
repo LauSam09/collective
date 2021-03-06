5287829afaeb7f532708ab11099d42d2c818633d
Laurence Ede <laurence.ede@gmail.com>
2021-03-06 14:50:18 +0000

    Fix test broken by routing changes

--- src/Layout/Content/Content.test.tsx
+++ src/Layout/Content/Content.test.tsx
@@ -1,9 +1,14 @@
 import { render } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
 
 import Content from "."
 
 describe("<Content />", () => {
   test("renders", () => {
-    render(<Content />)
+    render(
+      <BrowserRouter>
+        <Content />
+      </BrowserRouter>
+    )
   })
 })
