e28d9214d34aada5ec8a33a75da426f5108e5463
Laurence Ede <laurence.ede@gmail.com>
2021-03-07 15:14:32 +0000

    Display category

--- src/Lists/List/List.tsx
+++ src/Lists/List/List.tsx
@@ -7,19 +7,31 @@ export function List() {
   const addedItems = useListItems()
   const categories = useListCategories()
 
+  const groupedItems = categories.map((c) => ({
+    ...c,
+    items: addedItems.filter((i) => i.category === c.id),
+  }))
+
   return (
     <article>
       <section>
         {addedItems.length === 0 ? (
           <span>Nothing added yet!</span>
         ) : (
-          <div className={classes.list}>
-            {addedItems.map((item) => (
-              <ListItem
-                key={item.name}
-                item={item}
-                category={categories.find((c) => c.id === item.category)}
-              />
+          <div>
+            {groupedItems.map((c) => (
+              <div
+                key={c.id}
+                className={classes.category}
+                style={{ backgroundColor: `${c.colour}40` }}
+              >
+                <small>{c.name.toLocaleUpperCase()}</small>
+                <div className={classes.list}>
+                  {c.items.map((item) => (
+                    <ListItem key={item.name} item={item} category={c} />
+                  ))}
+                </div>
+              </div>
             ))}
           </div>
         )}
