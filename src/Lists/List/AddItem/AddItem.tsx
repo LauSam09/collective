3fa3b140467e0fcb542bfac24cdb79d39e3da032
Laurence Ede <laurence.ede@gmail.com>
2021-03-10 18:42:23 +0000

    Add generic Item component and used it in AddItem

--- src/Lists/List/AddItem/AddItem.tsx
+++ src/Lists/List/AddItem/AddItem.tsx
@@ -1,21 +1,13 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Item } from "../Common/Item"
 
 import classes from "./AddItem.module.css"
 
 export function AddItem() {
   return (
     <form onSubmit={(e) => e.preventDefault()} className={classes.form}>
-      <div className={classes.container}>
-        <input placeholder="Name #category" />
-        <button
-          style={{
-            backgroundColor: "#dedede",
-          }}
-        >
-          <FontAwesomeIcon icon={faBars} size="2x" />
-        </button>
-      </div>
+      <Item>
+        <input />
+      </Item>
       <div className={classes.actions}>
         <button type="button" className={classes.clear}>
           Clear
---
+++ src/Lists/List/Common/Item/Item.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+import classes from "./Item.module.css"
+
+type ItemProps = {
+  buttonColour?: string
+  children?: React.ReactNode
+}
+
+export function Item(props: ItemProps) {
+  const { buttonColour, children } = props
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>{children}</div>
+      <button style={{ backgroundColor: buttonColour || "inherit" }}>
+        <FontAwesomeIcon icon={faBars} size="2x" />
+      </button>
+    </div>
+  )
+}
+
+Item.defaultProps = {
+  buttonColour: "#dedede",
+}
