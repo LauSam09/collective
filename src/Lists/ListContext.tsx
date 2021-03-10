7e3d17d400a5408d0704445bc9f5841054d3d4e3
Laurence Ede <laurence.ede@gmail.com>
2021-03-10 21:04:00 +0000

    Add query snapshot for items

--- src/Lists/ListContext.tsx
+++ src/Lists/ListContext.tsx
@@ -10,6 +10,9 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 
 import { Category, Item } from "./models"
+import { useGroup } from "Authentication"
+
+const db = firebase.firestore()
 
 type ListContextType = {
   addedItems: Item[]
@@ -26,73 +29,44 @@ type ListContextProviderProps = {
 }
 
 export function ListContextProvider(props: ListContextProviderProps) {
-  const [addedItems, setAddedItems] = useState<Item[]>([
-    {
-      name: "Spinach",
-      lowerName: "spinach",
-      category: "1",
-      count: 2,
-      notes: "Young leaf",
-      added: true,
-      completed: false,
-    },
-    {
-      name: "Lemons",
-      lowerName: "lemons",
-      category: "1",
-      count: 1,
-      notes: "x3",
-      added: true,
-      completed: true,
-    },
-    {
-      name: "Eggs",
-      lowerName: "eggs",
-      category: "2",
-      count: 2,
-      notes: undefined,
-      added: true,
-      completed: true,
-    },
-    {
-      name: "Chocolate",
-      lowerName: "chocolate",
-      category: "",
-      count: 2,
-      notes: undefined,
-      added: true,
-      completed: true,
-    },
-    {
-      name: "Frozen peas",
-      lowerName: "frozen peas",
-      category: "3",
-      count: 3,
-      notes: "Fischer",
-      added: true,
-      completed: false,
-    },
-  ])
-  const [categories, setCategories] = useState<Category[]>([
-    {
-      id: "1",
-      name: "Fruit & Vegetables",
-      colour: "#74ac72",
-      order: 0,
-    },
-    {
-      id: "2",
-      name: "Dairy",
-      colour: "#fcf403",
-      order: 1,
-    },
-    {
-      id: "3",
-      name: "Frozen",
-      colour: "#0384fc",
-      order: 3,
-    },
-  ])
+  const { defaultList, id } = useGroup() || {}
+  const [addedItems, setAddedItems] = useState<Item[]>([])
+  const [categories, setCategories] = useState<Category[]>([])
+
+  if (!defaultList || !id) {
+    throw new Error("Group not defined for user.")
+  }
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection("groups")
+      .doc(id)
+      .collection("lists")
+      .doc(defaultList)
+      .collection("items")
+      .onSnapshot((snapshot) => {
+        const items: Item[] = []
+        snapshot.docChanges().forEach((change) => {
+          const { added, ...rest } = change.doc.data()
+          switch (change.type) {
+            case "added": {
+              added &&
+                items.push({
+                  ...rest,
+                  completed: !!rest.completed,
+                  id: change.doc.id,
+                } as Item)
+              break
+            }
+          }
+        })
+        setAddedItems(items)
+      })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [defaultList, id])
 
   return (
     <ListContext.Provider value={{ addedItems, categories }}>
@@ -100,6 +74,75 @@ export function ListContextProvider(props: ListContextProviderProps) {
     </ListContext.Provider>
   )
 }
+
+const fakeItems = [
+  {
+    name: "Spinach",
+    lowerName: "spinach",
+    category: "1",
+    count: 2,
+    notes: "Young leaf",
+    added: true,
+    completed: false,
+  },
+  {
+    name: "Lemons",
+    lowerName: "lemons",
+    category: "1",
+    count: 1,
+    notes: "x3",
+    added: true,
+    completed: true,
+  },
+  {
+    name: "Eggs",
+    lowerName: "eggs",
+    category: "2",
+    count: 2,
+    notes: undefined,
+    added: true,
+    completed: true,
+  },
+  {
+    name: "Chocolate",
+    lowerName: "chocolate",
+    category: "",
+    count: 2,
+    notes: undefined,
+    added: true,
+    completed: true,
+  },
+  {
+    name: "Frozen peas",
+    lowerName: "frozen peas",
+    category: "3",
+    count: 3,
+    notes: "Fischer",
+    added: true,
+    completed: false,
+  },
+]
+
+const fakeCategories = [
+  {
+    id: "1",
+    name: "Fruit & Vegetables",
+    colour: "#74ac72",
+    order: 0,
+  },
+  {
+    id: "2",
+    name: "Dairy",
+    colour: "#fcf403",
+    order: 1,
+  },
+  {
+    id: "3",
+    name: "Frozen",
+    colour: "#0384fc",
+    order: 3,
+  },
+]
 
 export const useListContext = () => useContext(ListContext)
 export const useListItems = () => useListContext().addedItems
--- src/Lists/models/item.ts
+++ src/Lists/models/item.ts
@@ -1,4 +1,5 @@
 export interface Item {
+  id: string
   added: boolean
   completed: boolean
   category: string
