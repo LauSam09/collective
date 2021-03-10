11484fdaee1dc73dd5c9cce22f4403f14a4ab733
Laurence Ede <laurence.ede@gmail.com>
2021-03-10 18:49:13 +0000

    Use Item in ListItem

--- src/Lists/List/ListItem/ListItem.tsx
+++ src/Lists/List/ListItem/ListItem.tsx
@@ -1,12 +1,13 @@
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import { Category, Item } from "../../models"
+import { Category, Item as ItemModel } from "../../models"
+import { Item } from "../Common/Item"
 
 import classes from "./ListItem.module.css"
 
 export type ListItemProps = {
-  item: Item
+  item: ItemModel
   category: Category | undefined
 }
 
@@ -14,19 +15,14 @@ export function ListItem(props: ListItemProps) {
   const { item, category } = props
 
   return (
-    <div className={classes.container}>
-      <input type="checkbox" checked={item.completed} />
-      <div className={classes.details}>
-        <span>{item.name}</span>
-        <small>{item.notes}</small>
+    <Item buttonColour={category?.colour}>
+      <div className={classes.content}>
+        <input type="checkbox" checked={item.completed} />
+        <div className={classes.details}>
+          <span>{item.name}</span>
+          <small>{item.notes}</small>
+        </div>
       </div>
-      <button
-        style={{
-          backgroundColor: category?.colour || "inherit",
-        }}
-      >
-        <FontAwesomeIcon icon={faBars} size="2x" />
-      </button>
-    </div>
+    </Item>
   )
 }
