027928315084c19fdcf397cff1ac2f8d07d859bc
Laurence Ede <laurence.ede@gmail.com>
2021-04-10 13:18:03 +0100

    Add remove and delete event handlers

--- src/Lists/List/ItemModal/ItemModal.tsx
+++ src/Lists/List/ItemModal/ItemModal.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react"
 import Modal from "react-modal"
+import firebase from "firebase"
 
 import { Item } from "Lists/models"
 
 import classes from "./ItemModal.module.css"
 import { useUserContext } from "Authentication"
-import { FullPageSpinner } from "Common"
 
 type ItemModalProps = {
   isOpen: boolean
@@ -17,23 +17,37 @@ export function ItemModal(props: ItemModalProps) {
   const { isOpen, item, close } = props
   const [notes, setNotes] = useState(item?.notes)
   const { getDefaultItemsCollection } = useUserContext()
-  const [saving, setSaving] = useState(false)
 
   useEffect(() => {
-    setSaving(false)
-    setNotes(item?.notes)
+    // Prevents old values being displayed when modal fades out.
+    if (isOpen) {
+      setNotes(item?.notes)
+    }
   }, [item, isOpen])
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    try {
+
+    if (notes !== item?.notes) {
       await getDefaultItemsCollection().doc(item?.id).update({
         notes,
       })
-      close()
-    } catch {
-      setSaving(false)
     }
+    close()
+  }
+
+  async function handleRemove() {
+    await getDefaultItemsCollection().doc(item?.id).update({
+      completed: false,
+      added: false,
+      notes: firebase.firestore.FieldValue.delete(),
+    })
+    close()
+  }
+
+  async function handleDelete() {
+    await getDefaultItemsCollection().doc(item?.id).delete()
+    close()
   }
 
   return (
@@ -44,23 +58,18 @@ export function ItemModal(props: ItemModalProps) {
       className={classes.modal}
     >
       <div className={classes.modalBody}>
-        {saving ? <FullPageSpinner /> : null}
         <h4>{item?.name}</h4>
         <form onSubmit={handleSubmit}>
           <label>Notes</label>
-          <input
-            disabled={saving}
-            value={notes}
-            onChange={(e) => setNotes(e.target.value)}
-          />
+          <input value={notes} onChange={(e) => setNotes(e.target.value)} />
           <h5>Actions</h5>
           <div>
-            <button disabled={saving} type="button">
+            <button type="button" onClick={handleRemove}>
               Remove from list
             </button>
           </div>
           <div>
-            <button disabled={saving} type="button">
+            <button type="button" onClick={handleDelete}>
               Delete permanently
             </button>
           </div>
@@ -68,9 +77,7 @@ export function ItemModal(props: ItemModalProps) {
             <button type="button" onClick={close}>
               Cancel
             </button>
-            <button disabled={saving} type="submit">
-              Save
-            </button>
+            <button type="submit">Save</button>
           </div>
         </form>
       </div>
