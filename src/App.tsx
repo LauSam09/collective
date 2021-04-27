085017f9f7f9216e1001de9b40d73219430f6898
Laurence Ede <laurence.ede@gmail.com>
2021-04-27 20:42:17 +0100

    Add dependency array for setting theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react"
 
 import { Content, Footer, Header } from "Layout"
+
 import classes from "./App.module.css"
 
 export function App() {
   useEffect(() => {
     document.body.dataset.theme = "light"
-  })
+  }, [])
 
   return (
     <div className={classes.app}>
