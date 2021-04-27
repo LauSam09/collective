ecbca68a0f14b08eb9197cfd9fcec03f8959f269
Laurence Ede <laurence.ede@gmail.com>
2021-04-27 21:02:21 +0100

    Add pwa support

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import Modal from "react-modal"
 
 import { App } from "App"
 import { AppProviders } from "./AppProviders"
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 import { reportWebVitals } from "reportWebVitals"
 import "index.css"
 
@@ -20,6 +21,15 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+if (process.env.NODE_ENV === "development") {
+  serviceWorkerRegistration.unregister()
+} else {
+  serviceWorkerRegistration.register()
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
