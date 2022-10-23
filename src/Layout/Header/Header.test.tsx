64209e13b1ab840c7b4ab7468412dd2dc57d7507
Laurence Ede <laurence.ede@gmail.com>
2022-10-23 15:49:07 +0100

    Remove snapshot test

--- src/Layout/Header/Header.test.tsx
+++ src/Layout/Header/Header.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
-import renderer from "react-test-renderer"
 
 import { Header } from "./Header"
 
@@ -13,11 +12,6 @@ function TestHeader() {
 }
 
 describe("<Header />", () => {
-  test("matches snapshot", () => {
-    const json = renderer.create(<TestHeader />).toJSON()
-    expect(json).toMatchSnapshot()
-  })
-
   test("renders name of application", () => {
     render(<TestHeader />)
     expect(screen.getByText(/collective/i)).toBeInTheDocument()
