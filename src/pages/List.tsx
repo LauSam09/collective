c45a9d1f6072fa7c5add65970237a6539dd60933
Laurence Ede <laurence.ede@gmail.com>
2023-01-22 18:44:21 +0000

    Add floating add button to list screen

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react"
+import { AddIcon } from "@chakra-ui/icons"
+import { Box, Button } from "@chakra-ui/react"
 
 import { Categories } from "../components/List/Categories"
 
@@ -6,6 +7,18 @@ export const ListPage = () => {
   return (
     <Box>
       <Categories />
+      <Box mb="12" />
+      <Button
+        variant="solid"
+        size="lg"
+        position="fixed"
+        colorScheme={"blue"}
+        bottom="4"
+        right="4"
+        zIndex={10}
+      >
+        <AddIcon />
+      </Button>
     </Box>
   )
 }
