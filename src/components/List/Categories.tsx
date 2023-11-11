eadba9410c2767dcd4285738931e2ecf5018cc11
Laurence Ede <laurence.ede@gmail.com>
2023-11-11 20:09:18 +0000

    Add clear completed button

--- src/components/List/Categories.tsx
+++ src/components/List/Categories.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { Skeleton, Stack, useDisclosure } from "@chakra-ui/react";
+import { Button, Flex, Skeleton, Stack, useDisclosure } from "@chakra-ui/react";
+import { deleteField, doc, writeBatch } from "firebase/firestore";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 import { Category } from "./Category";
 import { EditItemModal } from "./EditItemModal";
@@ -8,10 +10,13 @@ import { ItemDetailsModal } from "./ItemDetailsModal";
 import { Item as ItemModel } from "../../models/item";
 import { Category as CategoryModel } from "../../models/category";
 import { useList } from "../../hooks/useList";
+import { useAuthentication, useFirebase } from "../../hooks";
 
 export const Categories = () => {
   const detailsDisclosure = useDisclosure();
   const editDisclosure = useDisclosure();
+  const { firestore } = useFirebase();
+  const { appUser } = useAuthentication();
   const [selectedItem, setSelectedItem] = useState<ItemModel>();
   const { isLoading: loading, categories, addedItems: items } = useList();
 
@@ -23,6 +28,30 @@ export const Categories = () => {
   const handleOpenEdit = (item: ItemModel) => {
     setSelectedItem(item);
     editDisclosure.onOpen();
+  };
+
+  const handleClickClear = () => {
+    const batch = writeBatch(firestore);
+
+    for (const item of items.filter((item) => item.completed)) {
+      const itemRef = doc(
+        firestore,
+        "groups",
+        appUser!.group.id,
+        "lists",
+        appUser!.group.defaultList,
+        "items",
+        item.id,
+      );
+      batch.update(itemRef, {
+        completed: false,
+        added: false,
+        notes: deleteField(),
+        flagged: deleteField(),
+      });
+    }
+
+    batch.commit();
   };
 
   if (loading) {
@@ -52,6 +81,11 @@ export const Categories = () => {
 
   return (
     <>
+      <Flex justify="flex-end" mb={2}>
+        <Button colorScheme="red" onClick={handleClickClear}>
+          <DeleteIcon />
+        </Button>
+      </Flex>
       <Stack>
         {displayCategories.map((category) => (
           <Category key={category.name} {...category}>
