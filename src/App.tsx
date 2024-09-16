a39c36da4be88177a42e607efd9416a8c3f0bce1
Laurence Ede <laurence.ede@gmail.com>
2024-09-16 20:14:56 +0100

    Add initial firebase configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,49 @@
-export const App = () => <div>Hello, World!</div>;
+import { useEffect, useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "@/firebase";
+
+export const App = () => {
+  console.log(app); //  TODO: This is a bodge to ensure the app is initialised. Need to find a better way to do this.
+  const [state, setState] = useState<
+    "loading" | "unauthenticated" | "authenticated"
+  >("loading");
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setState("authenticated");
+      } else {
+        setState("unauthenticated");
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const signIn = async () => {
+    const provider = new GoogleAuthProvider();
+
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signOut = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>state: {state}</div>
+      {state === "unauthenticated" && <button onClick={signIn}>Sign in</button>}
+      {state === "authenticated" && <button onClick={signOut}>Sign out</button>}
+    </div>
+  );
+};
