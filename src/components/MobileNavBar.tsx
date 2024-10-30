81205d8bec261f7dba8068bcbbd13a82137c6b42
Laurence Ede <laurence.ede@gmail.com>
2024-10-30 15:58:41 +0000

    Refactor mobile nav bar

--- src/components/MobileNavBar.tsx
+++ src/components/MobileNavBar.tsx
@@ -13,13 +13,15 @@ import {
   Show,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { NavLink } from "react-router-dom";
+import { NavLink, NavLinkProps } from "react-router-dom";
 
 export const MobileNavBar = () => {
   const backgroundColor = useColorModeValue("gray.100", "gray.700");
 
   return (
     <Show below="sm">
+      {/* Prevent mobile nav bar from overlapping content at bottom of screen */}
+      <Box py={7} />
       <Box
         position="fixed"
         bottom={0}
@@ -28,146 +30,87 @@ export const MobileNavBar = () => {
         backgroundColor={backgroundColor}
       >
         <HStack alignItems="stretch" gap={1}>
-          {/* TODO: Factor out these components */}
-          <Link
-            as={NavLink}
-            to="/"
-            p={2}
-            borderRadius="md"
-            flex={1}
-            display="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
-            _hover={{
-              textDecor: "none",
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-            _dark={{
-              _hover: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-              _activeLink: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-            }}
-            _activeLink={{
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-          >
+          <MobileNavLink to="/">
             <CheckIcon />
-          </Link>
-          <Link
-            as={NavLink}
-            to="/planning"
-            p={2}
-            borderRadius="md"
-            flex={1}
-            display="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
-            _hover={{
-              textDecor: "none",
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-            _dark={{
-              _hover: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-              _activeLink: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-            }}
-            _activeLink={{
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-          >
-            <CalendarIcon />
-          </Link>
+          </MobileNavLink>
 
-          <Button
-            colorScheme="blue"
-            variant="solid"
-            display="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
-            flex={1}
-            paddingY={4}
-            aria-label="Add item"
-            height="100%"
-            fontWeight="normal"
-            onClick={() => {}}
-            _hover={{
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-            _dark={{
-              _hover: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-            }}
-          >
-            <AddIcon />
-          </Button>
-          <Link
-            as={NavLink}
-            to="/recipes"
-            p={2}
-            borderRadius="md"
-            flex={1}
-            display="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
-            _hover={{
-              textDecor: "none",
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-            _dark={{
-              _hover: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-              _activeLink: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-            }}
-            _activeLink={{
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-          >
+          <MobileNavLink to="/planning">
+            <CalendarIcon />
+          </MobileNavLink>
+
+          <QuickAddButton />
+
+          <MobileNavLink to="/recipes">
             <SearchIcon />
-          </Link>
-          <Link
-            as={NavLink}
-            to="/settings"
-            p={2}
-            borderRadius="md"
-            flex={1}
-            display="flex"
-            flexDir="column"
-            alignItems="center"
-            justifyContent="center"
-            _hover={{
-              textDecor: "none",
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-            _dark={{
-              _hover: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-              _activeLink: {
-                backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
-              },
-            }}
-            _activeLink={{
-              backgroundColor: "var(--chakra-colors-gray-200)",
-            }}
-          >
+          </MobileNavLink>
+
+          <MobileNavLink to="/settings">
             <SettingsIcon />
-          </Link>
+          </MobileNavLink>
         </HStack>
       </Box>
     </Show>
   );
 };
+
+type MobileNavLinkProps = Pick<NavLinkProps, "to"> & {
+  children: React.ReactNode;
+};
+
+const MobileNavLink = ({ to, children }: MobileNavLinkProps) => (
+  <Link
+    as={NavLink}
+    to={to}
+    p={2}
+    borderRadius="md"
+    flex={1}
+    display="flex"
+    flexDir="column"
+    alignItems="center"
+    justifyContent="center"
+    _hover={{
+      textDecor: "none",
+      backgroundColor: "var(--chakra-colors-gray-200)",
+    }}
+    _dark={{
+      _hover: {
+        backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
+      },
+      _activeLink: {
+        backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
+      },
+    }}
+    _activeLink={{
+      backgroundColor: "var(--chakra-colors-gray-200)",
+    }}
+  >
+    {children}
+  </Link>
+);
+
+const QuickAddButton = () => (
+  <Button
+    colorScheme="blue"
+    variant="solid"
+    display="flex"
+    flexDir="column"
+    alignItems="center"
+    justifyContent="center"
+    flex={1}
+    paddingY={4}
+    aria-label="Add item"
+    height="100%"
+    fontWeight="normal"
+    onClick={() => {}}
+    _hover={{
+      backgroundColor: "var(--chakra-colors-gray-200)",
+    }}
+    _dark={{
+      _hover: {
+        backgroundColor: "var(--chakra-colors-whiteAlpha-200)",
+      },
+    }}
+  >
+    <AddIcon />
+  </Button>
+);
