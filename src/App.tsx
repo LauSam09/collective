f502166a49f476accd7b69af4c12dbfa89f1a374
Laurence Ede <laurence.ede@gmail.com>
2024-11-08 16:04:58 +0000

    Add button example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import { NavBar, MobileNavBar, List } from "@/components";
+import { Button } from "./components/ui/button";
 
 export const App = () => (
   <div>
     <NavBar />
     <Box p={4}>
+      <Button>Click me</Button>
       <Routes>
         <Route path="/" element={<List />} />
         <Route path="/recipes" element={<>Recipes</>} />
